12bffa45f913f20acb1a75f62e6a1789010db92f
tobyflowers <tobiasgflowers@gmail.com>
2023-08-01 16:22:03 -0400

    pog

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import cv2
 import april_tag
 
 img = cv2.imread('testframe.jpg', cv2.IMREAD_GRAYSCALE)
-folder = "/home/toby/code/apriltag_following/"
+folder = "./"
 output_path = os.path.join(folder, f"img.jpg")
 print(april_tag.process_center_avg(img))
 cv2.imwrite(output_path, img)
