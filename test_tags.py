e8aac2e3018d6120ae06d322dab0e5d7bc751cfb
Alex <aexdremov@gmail.com>
2023-08-02 00:08:19 -0400

    adds test get tags, test img with no tags

--- test_tags.py
+++ test_tags.py
@@ -5,17 +5,22 @@ import april_tag
 
 class TestAprilsTags(unittest.TestCase):
 
-    #ALEX
+    # alex
     def test_get_tags(self):
-        self.assertRaises(TypeError, lambda: april_tag.get_tags(None))
-        self.assertNotEqual(april_tag.get_tags())
+        # can only get two for now, will add more in class
+        self.assertRaises(TypeError, lambda: april_tag.get_tags(None)) # check if type error is raised 
+        self.assertEqual(april_tag.get_tags('no_tag.png'), None) # check if using an image with no tags returns the right stuff
     
     #CHARLOTTE
     def test_PID(self):
         self.assertEquals(april_tag.output_from_tags(), 0, 0)
 
-    #DANIELA
+    # idk
     def test_frames(self):
+        '''
+        make sure you get the right amount of frames and width/size 
+        for a frame in a video, maybe make a 30 frame test case
+        '''
         pass
 
     #TOBY
